ce637f1a89986536f8b94035a892d3a69f9d8c6c
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2022-03-20 19:53:16 -0300

    Nuevos avances

--- src/app/web/alquileres/alquileres.component.ts
+++ src/app/web/alquileres/alquileres.component.ts
@@ -8,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlquileresComponent implements OnInit {
 
+  public elementos = [
+    { 
+      detalles: 'Potrero de los funes', 
+      tipo: 'casa', 
+      venta: false, 
+      ubicacion: 'Potrero de los Funes (San Luis)', 
+      precio: {
+        valor: 12000000,
+        usd: true,
+      },
+      codigo: 'CA001',
+      img: 'https://www.serargentino.com/public/images/2020/10/16038162220-Potrero-de-los-Funes-773x458.jpg' 
+    },
+    { 
+      detalles: 'La florida', 
+      tipo: 'departamento', 
+      venta: false, 
+      ubicacion: 'La florida (San Luis)', 
+      precio: {
+        valor: 30000,
+        usd: true,
+      },
+      codigo: 'DE001',
+      img: 'https://tripin.travel/wp-content/uploads/2016/10/Dique-La-Florida-San-Luis-Argentina.jpg' 
+    },
+    { 
+      detalles: 'Trapiche', 
+      tipo: 'terreno', 
+      venta: false, 
+      ubicacion: 'El trapiche (San Luis)', 
+      precio: {
+        valor: 12000000,
+        usd: false,
+      },
+      codigo: 'TE001',
+      img: 'https://www.serargentino.com/public/images/2020/10/16037372200-trapiche-773x458.jpg' 
+    },
+    { 
+      detalles: 'Cruz de piedra', 
+      tipo: 'lote', 
+      venta: false, 
+      ubicacion: 'Cruz de piedra (San Luis)', 
+      precio: {
+        valor: 20000,
+        usd: true,
+      }, 
+      codigo: 'LT001',
+      img: 'https://viapais.com.ar/resizer/kWBOqktVGS2GzANnHlI3UPEtZLU=/982x551/smart/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/MZSTINLGGJTDCMRRGZSDSZJTMY.jpg' 
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
